c3a558829f1bb757fc4ffbfb93970b1c63cdafc6
David meilleur <meilleurd2001@gmail.com>
2026-08-19 22:45:42 +0200

    Load test dataset from IMDB for evaluation

--- src/train.py
+++ src/train.py
@@ -17,7 +17,7 @@ SAMPLE_SIZE = 5000  # 5000 exemples pour rester rapide
 
 def main():
     # Chargement du dataset IMDB
-    dataset = load_dataset("stanfordnlp/imdb", split=f"train[:{SAMPLE_SIZE}]")
+    test_dataset = load_dataset("stanfordnlp/imdb", split="test[:1000]")
     tokenizer = DistilBertTokenizer.from_pretrained(MODEL_NAME)
 
     def tokenize_function(examples):
