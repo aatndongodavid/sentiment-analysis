938af238d45d23781367b4d994b8444901e923b6
David meilleur <meilleurd2001@gmail.com>
2026-08-19 17:05:11 +0200

    Reorder import statements in test_train.py

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,8 +1,9 @@
 """
 Test simple pour vérifier que le script d'entraînement peut être importé sans erreur.
 """
-import sys
 import os
+import sys
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "src"))
 
 def test_train_importable():
